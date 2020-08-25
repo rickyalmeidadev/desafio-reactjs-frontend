3ab768d5ea8986f0d764cfdae498d20e178d0d66
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-08-25 11:39:54 -0300

    feat: create Home page with styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
+import { Header, Button, NaverCard } from '../../components';
+import { Container, Content, Main, ControlsSection, Grid } from './styles';
+
 const Home: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Content>
+        <Header />
+        <Main>
+          <ControlsSection>
+            <h1>Navers</h1>
+            <Button type="button">Adicionar Naver</Button>
+          </ControlsSection>
+          <Grid>
+            <NaverCard />
+            <NaverCard />
+            <NaverCard />
+            <NaverCard />
+            <NaverCard />
+            <NaverCard />
+            <NaverCard />
+            <NaverCard />
+          </Grid>
+        </Main>
+      </Content>
+    </Container>
+  );
 };
 
 export default Home;
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: ${props => props.theme.maxWidth};
+
+  margin: 0 auto;
+  padding: ${props => props.theme.paddings.normal};
+`;
+
+export const Main = styled.main`
+  width: 100%;
+`;
+
+export const ControlsSection = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-bottom: ${props => props.theme.margins.larger};
+
+  > h1 {
+    font-size: 4rem;
+    font-weight: 600;
+  }
+`;
+
+export const Grid = styled.section`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (min-width: 450px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: ${props => props.theme.margins.larger};
+  }
+
+  @media (min-width: 626px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: ${props => props.theme.margins.larger};
+  }
+
+  @media (min-width: 824px) {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: ${props => props.theme.margins.larger};
+  }
+`;
