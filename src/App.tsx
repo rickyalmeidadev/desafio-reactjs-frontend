0146d20ce0fc07d08fc0397ed5d551630b9de163
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-08-25 10:45:45 -0300

    feat: setup App with routes and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from './assets/styles/global';
+import theme from './assets/styles/theme';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
-  return <h1>Navedex</h1>;
-}
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Routes />
+    </ThemeProvider>
+  );
+};
 
 export default App;
