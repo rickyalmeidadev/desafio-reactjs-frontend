592f9ff362b8ba8f90258e1a89ea01053e0d5f51
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-08-25 19:46:04 -0300

    feat: add logout handler in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { Container, Logo, Logout } from './styles';
 
 import logo from '../../assets/images/logo.png';
 
 const Header: React.FC = () => {
+  const history = useHistory();
+
+  const handleLogout = () => {
+    localStorage.removeItem('nave.rs:token');
+    history.push('/login');
+  };
+
   return (
     <Container>
       <Logo src={logo} alt="nave.rs" />
-      <Logout type="button">Sair</Logout>
+      <Logout type="button" onClick={handleLogout}>
+        Sair
+      </Logout>
     </Container>
   );
 };
