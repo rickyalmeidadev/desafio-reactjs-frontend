a451efe2b13ba6ac327d824d13b096b00e5f5194
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-08-25 11:34:32 -0300

    feat: create Login page with styles

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import { Field, Button } from '../../components';
+import { Container, Content, Form, Logo } from './styles';
+
+import logo from '../../assets/images/logo.png';
+
 const Login: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Content>
+        <Form>
+          <Logo src={logo} alt="nave.rs" />
+          <Field type="text" label="E-mail" name="email" placeholder="E-mail" />
+          <Field
+            type="password"
+            label="Senha"
+            name="password"
+            placeholder="Senha"
+          />
+          <Button fullWidth type="submit">
+            Entrar
+          </Button>
+        </Form>
+      </Content>
+    </Container>
+  );
 };
 
 export default Login;
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: ${props => props.theme.maxWidth};
+  min-height: 100vh;
+
+  margin: 0 auto;
+  padding: ${props => props.theme.paddings.normal};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Form = styled.form`
+  height: 408px;
+  width: 100%;
+  max-width: 448px;
+
+  border: 1px solid ${props => props.theme.colors.shade};
+
+  padding: ${props => props.theme.paddings.larger};
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Logo = styled.img`
+  margin-bottom: ${props => props.theme.margins.larger};
+`;
