bc542ceca94ba9382c1be8a0d9e95a78aa005b2d
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-08-26 14:22:43 -0300

    feat: verify token in private routes

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Redirect, useLocation, RouteProps } from 'react-router-dom';
-import { api } from '../services/api';
+
+import { PageLoader } from '../components';
+
+import { api, verify } from '../services/api';
 
 interface Props extends RouteProps {
   component: React.FC;
 }
 
 const PrivateRoute: React.FC<Props> = ({ component: Component, ...rest }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   const token = localStorage.getItem('nave.rs:token');
+
+  useEffect(() => {
+    if (!token) {
+      setIsLoading(false);
+      return;
+    }
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+    (async () => {
+      try {
+        await verify();
+        setIsAuthenticated(true);
+      } catch (error) {
+        localStorage.removeItem('nave.rs:token');
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [token]);
 
   const location = useLocation();
 
-  if (!token) {
-    return <Redirect to={{ pathname: '/login', state: { from: location } }} />;
-  }
-
-  api.defaults.headers.Authorization = `Bearer ${token}`;
-
-  return <Route {...rest} component={Component} />;
+  return isLoading ? (
+    <PageLoader />
+  ) : isAuthenticated ? (
+    <Route {...rest} component={Component} />
+  ) : (
+    <Redirect to={{ pathname: '/login', state: { from: location } }} />
+  );
 };
 
 export default PrivateRoute;
